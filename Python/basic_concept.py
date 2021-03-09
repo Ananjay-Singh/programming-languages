f8d55803627fb5ff19c4f4d37d83ce107c2e37c9
Ananjay Singh <ananjay.bigdata@gmail.com>
2021-03-09 11:04:44 +0530

    Added basic python coding

---
+++ Python/basic_concept.py
@@ -0,0 +1,41 @@
+Why Python:
+    - Simple to learn
+    - Simple to Use
+    - Great Community
+    - High Demand
+    - Widly Used
+        - Web Development
+        - Data Science
+            - BigData
+            - Machine Learning
+        - Education and Learning
+        - Scripting
+What is python:
+    - Unique Syntax
+    - Genral-purpose
+    - Multi-paradigm
+    - Interpreted
+    - Garbage-collected
+    - Dynamically-typed
+Pros:
+    - Comprehensive Standard Library
+        - Collections
+        - File I/O
+        - Dates and times
+        - Compression
+        - User Interfaces
+        - Much More .....
+    - Community-driven (PEPs- Python Enhancement Purposal)
+    - 3rd Party Libraries (PyPI- Python Package Index)
+        - Install/Uninstall - using pip
+    - 3rd Party Tools
+        - IDE- Pydev, Pycharm, Visal Studio Code, Spyder,
+        - Editors - Sublime, Vim, GNU/Emacs
+        - Python Code Tools
+            - Style Guide Enforcement (flake8)
+            - Code Analysis (Pylint)
+            - Code Formatter (black)
+Cons:
+    - Interpreted (slow)
+    - Not Native (High memory usage, lack of native security sandbox)
+    - Dynamic (Runtime errors)
